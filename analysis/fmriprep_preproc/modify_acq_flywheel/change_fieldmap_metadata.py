bf535d49ae72847fbc2689f466f1ee308179b9f4
kmbond <kristambond@gmail.com>
2020-12-13 19:33:59 -0500

    update fieldmap metadata

---
+++ analysis/fmriprep_preproc/modify_acq_flywheel/change_fieldmap_metadata.py
@@ -0,0 +1,87 @@
+"""
+
+This script changes the phase encoding direction and modality label
+for all the field map acquisitions of a certain project in Flywheel.
+
+"""
+
+import flywheel
+from os.path import join as opj
+
+# Initialise first flywheel client
+api_key = "bridge-center.flywheel.io:Y86teOyF7LfgZ2yLb9"
+fw = flywheel.Client(api_key)
+
+
+
+# Define project and look them up on the client
+lab = "coax"
+project_label = "LOKI1"
+
+project = fw.lookup((opj(lab, project_label)).replace('\\','/'))
+
+# crete and format the query to find acquisitions
+query = (
+    'label=~.*fmap,'
+    'parents.project={}'
+)
+
+
+query = query.format(project.id)
+
+# Find acquisitions matching query
+acq_list = fw.acquisitions.find(query)
+
+
+# Acq should be epi
+# modality should be either Magnitude1 or phasediff
+
+# Iterate over returned acquisitions
+for acq in acq_list:
+    # Iterate over all files in that acquisition
+    for file in acq.files:
+
+        if file['type'] == 'nifti':
+
+            if file['classification']['Intent'][0] == 'Fieldmap':
+                print('modifying the metadata for ' + file['classification']['Intent'][0])
+                print('session %s of subject %s '
+                              'in project %s' % (fw.get(acq['parents'].session).label,
+                                                 fw.get(acq['parents'].subject).label,
+                                                 project_label))
+
+                # Initialise update dict
+                update_dict = dict()
+                #get subject and session label
+                subject_label = fw.get(acq['parents'].subject).label
+                session_label = fw.get(acq['parents'].session).label
+                # For custom info that is hierarchical, we must copy
+                bids_dict = file['info'].get('BIDS')
+                print('BIDS dict ', bids_dict)
+                # Only do something if BIDS exists on file
+                if isinstance(bids_dict, dict):
+                    bids_dict['Acq'] = "epi"
+
+                    if 'phasediff' in bids_dict['Filename']:
+                        print('phasediff fieldmap found')
+
+                        bids_dict['Modality'] = 'phasediff'
+
+                    elif 'magnitude' in bids_dict['Filename']:
+                        print('magnitude fieldmap found')
+
+                        bids_dict['Modality'] = 'Magnitude1'
+
+                    update_dict['BIDS'] = bids_dict
+                    # For flat key-value pairs, no need to copy - just set
+                    # update_dict['PhaseEncodingDirection'] = mew_ph_enc_dir
+                    # Uncomment below to actually update
+                    acq.update_file_info(file['name'], update_dict)
+
+                    print("fieldmap info update finished")
+                else:
+                    print('there is no BIDS info for session %s of subject %s '
+                          'in project %s' % (fw.get(acq['parents'].session).label,
+                                             fw.get(acq['parents'].subject).label,
+                                             project_label)
+                          )
